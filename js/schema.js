b18cd513c7ebc40fb185dda684eb29313bd2c985
Hung Tran <nguyentieuhau@gmail.com>
2022-09-26 20:27:01 +0700

    Product review and rating

--- js/schema.js
+++ js/schema.js
@@ -1,13 +1,34 @@
-function schemaToggleAggregateRating() {
+function schemaToggleProductAggregateRating() {
     var toggle = $('input[name="data[header][product][aggregate_rating]"]:checked').val();
 
-    $('.aggregate-rating-toggle').toggle((toggle == 1));
+    $('.product-aggregate-rating-toggle').toggle((toggle == 1));
+}
+
+function schemaToggleProductReviewRating(el) {
+    var toggle = el.val();
+
+    el.closest('.form-field').siblings('.review-toggle').toggle((toggle == 1));
 }
 
 $(document).ready(function() {
-    schemaToggleAggregateRating();
+    schemaToggleProductAggregateRating();
+
+    var productReviewToggles = $('input[type="radio"][name^="data[header][product][reviews]"]:checked');
+
+    if (productReviewToggles.length > 0) {
+        for (var i = 0; i < productReviewToggles.length; i++) {
+            var el = productReviewToggles[i];
+            console.log(el);
+            schemaToggleProductReviewRating($(el));
+        }
+    }
 
     $('input[name="data[header][product][aggregate_rating]"]').on('change', function() {
-        schemaToggleAggregateRating();
+        schemaToggleProductAggregateRating();
     });
+
+    $('body').on('change', 'input[type="radio"][name^="data[header][product][reviews]"]', function(e) {
+        schemaToggleProductReviewRating($(e.currentTarget));
+    });
+
 });
